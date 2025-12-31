8bedb0c5847ac4752046cc8360eaa71f3844bd99
Maarten Kieft <maarten.acc.github@kieft.ws>
2025-12-31 17:55:46 +0100

    Adding nextjs application

--- src/Wegrep.AppHost/AppHost.cs
+++ src/Wegrep.AppHost/AppHost.cs
@@ -1,5 +1,11 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.Wegrep_Api>("api");
+
+var app = builder.AddJavaScriptApp("app", "../Wegrep.App", "dev")
+    .WaitFor(api)
+    .WithReference(api)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
